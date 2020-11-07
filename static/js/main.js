3a0b4cf059bc121af4ee582ae5906f2e9f4d75bd
sundartamang <sundartamang6@gmail.com>
2020-11-07 08:18:13 +0545

    first commit

---
+++ static/js/main.js
@@ -0,0 +1,39 @@
+$(function () {
+    var navbar = $('.navbar');
+
+    $(window).scroll(function () {
+        if ($(window).scrollTop() <= 40) {
+            // navbar.removeClass('navbar-scroll');
+            $('.navbar').removeClass('navbar-scroll');
+        } else {
+            // navbar.addClass('navbar-scroll');
+            $('.navbar').addClass('navbar-scroll');
+        }
+    });
+});
+
+// paralex js
+
+// end of paralex js
+
+// for image gallery
+// jQuery(document).ready(function () {
+//     lightbox.option({
+//         'resizeDuration': 200,
+//         'wrapAround': true,
+//         'alwaysShowNavOnTouchDevices': true,
+//         'fitImagesInViewport': true,
+//     });
+// });
+// magnigfic gallery javascript
+jQuery(document).ready(function () {
+    $('.gallerys').magnificPopup({
+        type: 'image',
+        delegate: 'a',
+        gallery: {
+            enabled: true
+        },
+    });
+});
+// magnigfic gallery javascript end
+
